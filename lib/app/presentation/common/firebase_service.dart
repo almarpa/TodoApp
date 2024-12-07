b75aa371fcc41898c83b5d3fbd96e352e0e4dc17
amarcopa <amarcopa@mercadona.es>
2024-12-07 20:46:54 +0100

    feat(firebase): refactor code

--- lib/app/presentation/common/firebase_service.dart
+++ lib/app/presentation/common/firebase_service.dart
@@ -8,15 +8,6 @@ class FirebaseService {
 
   static Future initializeApp() async {
     await Firebase.initializeApp();
-    // await Firebase.initializeApp(
-    //     options: const FirebaseOptions(
-    //   apiKey: "AIzaSyBmLLBiitO9YkH424Q5wdInxR72cbberFQ",
-    //   appId: "1:351145816976:android:05b094bfc9890ae3a8f915",
-    //   messagingSenderId: "351145816976",
-    //   projectId: "todoapp-16793",
-    // ));
-
     await _firebaseMessaging.requestPermission();
-    // token = await _firebaseMessaging.getToken();
   }
 }
