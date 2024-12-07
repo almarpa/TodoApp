e3df5905cb27b9edfa25ab173c09a8b76e017afe
amarcopa <amarcopa@mercadona.es>
2024-12-07 22:09:04 +0100

    feat(firebase): refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_app/app/data/repository/impl/user_repository_impl.dart';
 import 'package:todo_app/my_app.dart';
-import 'app/presentation/common/firebase_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   Intl.defaultLocale = 'es_ES';
   await initializeDateFormatting();
+
   WidgetsFlutterBinding.ensureInitialized();
-  await FirebaseService.initializeApp();
+
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.requestPermission();
 
   runApp(MyApp(UserRepositoryImpl()));
 }
